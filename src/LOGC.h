ab34de9faa6c0afbe19cb2d8a910bd7eb815fa61
calvin <calvinwilliams.c@gmail.com>
2014-07-19 22:36:38 +0800

    UPDATE TO V1.1.0D

--- src/LOGC.h
+++ src/LOGC.h
@@ -2,10 +2,10 @@
 #define _H_LOGC_
 
 /*
- * LOG - 轻便型日志函数库
- * author      : calvin
- * email       : calvinwilliams.c@gmail.com
- * LastVersion : 2014-04-27	v1.0.1		创建
+ * iLOG3Lite - log function library written in c
+ * author	: calvin
+ * email	: calvinwilliams.c@gmail.com
+ * LastVersion	: v1.0.9
  *
  * Licensed under the LGPL v2.1, see the file LICENSE in base directory.
  */
@@ -62,6 +62,7 @@ extern "C" {
 #define CLOSE		close
 #define PROCESSID	(unsigned long)getpid()
 #define THREADID	(unsigned long)pthread_self()
+#define NEWLINE		"\n"
 #elif ( defined _WIN32 )
 #define TLS		__declspec( thread )
 #define VSNPRINTF	_vsnprintf
@@ -72,6 +73,7 @@ extern "C" {
 #define CLOSE		_close
 #define PROCESSID	(unsigned long)GetCurrentProcessId()
 #define THREADID	(unsigned long)GetCurrentThreadId()
+#define NEWLINE		"\r\n"
 #endif
 
 /* 简单日志函数 */
@@ -85,11 +87,34 @@ extern "C" {
 
 void SetLogFile( char *format , ... );
 void SetLogLevel( int log_level );
+
 int FatalLog( char *c_filename , long c_fileline , char *format , ... );
 int ErrorLog( char *c_filename , long c_fileline , char *format , ... );
 int WarnLog( char *c_filename , long c_fileline , char *format , ... );
 int InfoLog( char *c_filename , long c_fileline , char *format , ... );
 int DebugLog( char *c_filename , long c_fileline , char *format , ... );
+
+int FatalHexLog( char *c_filename , long c_fileline , char *buf , long buflen , char *format , ... );
+int ErrorHexLog( char *c_filename , long c_fileline , char *buf , long buflen , char *format , ... );
+int WarnHexLog( char *c_filename , long c_fileline , char *buf , long buflen , char *format , ... );
+int InfoHexLog( char *c_filename , long c_fileline , char *buf , long buflen , char *format , ... );
+int DebugHexLog( char *c_filename , long c_fileline , char *buf , long buflen , char *format , ... );
+
+#if ( defined __STDC_VERSION__ ) && ( __STDC_VERSION__ >= 199901 )
+
+#define FATALLOG(...)			FatalLog( __FILE__ , __LINE__ , __VA_ARGS__ );
+#define ERRORLOG(...)			ErrorLog( __FILE__ , __LINE__ , __VA_ARGS__ );
+#define WARNLOG(...)			WarnLog( __FILE__ , __LINE__ , __VA_ARGS__ );
+#define INFOLOG(...)			InfoLog( __FILE__ , __LINE__ , __VA_ARGS__ );
+#define DEBUGLOG(...)			DebugLog( __FILE__ , __LINE__ , __VA_ARGS__ );
+
+#define FATALHEXLOG(_buf_,_buflen_,...)	FatalHexLog( __FILE__ , __LINE__ , buf , buflen , __VA_ARGS__ );
+#define ERRORHEXLOG(_buf_,_buflen_,...)	ErrorHexLog( __FILE__ , __LINE__ , buf , buflen , __VA_ARGS__ );
+#define WARNHEXLOG(_buf_,_buflen_,...)	WarnHexLog( __FILE__ , __LINE__ , buf , buflen , __VA_ARGS__ );
+#define INFOHEXLOG(_buf_,_buflen_,...)	InfoHexLog( __FILE__ , __LINE__ , buf , buflen , __VA_ARGS__ );
+#define DEBUGHEXLOG(_buf_,_buflen_,...)	DebugHexLog( __FILE__ , __LINE__ , buf , buflen , __VA_ARGS__ );
+
+#endif
 
 #ifdef __cplusplus
 }
--- src/tcpdaemon.h
+++ src/tcpdaemon.h
@@ -111,30 +111,56 @@ typedef int func_tcpmain( void *param_tcpmain , int sock , struct sockaddr *addr
 /* 主入口参数结构 */
 struct TcpdaemonEntryParam
 {
-	int		daemon_level ;
+	int		daemon_level ;	/* 是否转化为守护服务 1:转化 0:不转化（缺省） */
+					/* Whether into daemon service 1:YES 0:NO(DEFAULT) */
 	
-	char		log_pathfilename[ MAXLEN_FILENAME + 1 ] ;
-	int		log_level ;
+	char		log_pathfilename[ MAXLEN_FILENAME + 1 ] ;	/* 日志输出文件名，不设置则输出到标准输出上 */
+									/* default to standard output  */
+	int		log_level ;	/* 日志等级，不设置则缺省DEBUG等级 */
+					/* default the DEBUG level */
 	
-	int		call_mode ;	/* LF 领导者-追随者预派生进程池模型 for UNIX,Linux
-					   IF 即时派生进程模型 for UNIX,Linux
-					   WIN-TLF 领导者-追随者预派生线程池模型 for win32
+	int		call_mode ;	/* 应用接口模式
+					   TCPDAEMON_CALLMODE_DAEMON:主守护模式
+					   TCPDAEMON_CALLMODE_MAIN:函数调用模式
+					*/
+					/* Application interface pattern
+					   TCPDAEMON_CALLMODE_DAEMON:Main guardian mode
+					   TCPDAEMON_CALLMODE_MAIN:Call function mode
 					*/
 	
-	char		server_model[ 10 + 1 ] ;
-	long		max_process_count ;
-	long		max_requests_per_process ;
-	char		ip[ 20 + 1 ] ;
-	long		port ;
-	char		so_pathfilename[ MAXLEN_FILENAME + 1 ] ;
+	char		server_model[ 10 + 1 ] ;	/* TCP连接管理模型
+							LF:领导者-追随者预派生进程池模型 for UNIX,Linux
+							IF:即时派生进程模型 for UNIX,Linux
+							WIN-TLF:领导者-追随者预派生线程池模型 for win32
+							*/
+							/* TCP connection management model
+							LF:Leader-Follower process pool model for UNIX,Linux
+							IF:Instant derived process model for UNIX,Linux
+							WIN-TLF:Leader-Follower thread pool model for win32
+							*/
+	long		max_process_count ;	/* 当为领导者-追随者预派生进程池模型时为工作进程池进程数量，当为即时派生进程模型时为最大子进程数量 */
+						/* work process amount on Leader-Follower process pool model , or maximum work process amount on instant derived process model */
+	long		max_requests_per_process ;	/* 当为领导者-追随者预派生进程池模型时为单个工作进程最大处理应用次数 */
+							/* work process maximum application processing times on Leader-Follower process pool model */
+	char		ip[ 20 + 1 ] ;	/* 本地侦听IP */
+					/* local listen IP */
+	long		port ;	/* 本地侦听PORT */
+				/* local listen PORT */
+	char		so_pathfilename[ MAXLEN_FILENAME + 1 ] ;	/* 用绝对路径或相对路径表达的应用动态库文件名 */
+									/* file name of dynamic link library */
 	
-	func_tcpmain	*pfunc_tcpmain ;
-	void		*param_tcpmain ;
+	func_tcpmain	*pfunc_tcpmain ;	/* 当函数调用模式时，指向把TCP连接交给应用入口函数指针 */
+						/* tcpmain function pointer */
+	void		*param_tcpmain ;	/* 当函数调用模式时，指向把TCP连接交给应用入口函数的参数指针 */
+						/* tcpmain function parameter */
 	
-	int		tcp_nodelay ;
-	int		tcp_linger ;
-
+	int		tcp_nodelay ;	/* 启用TCP_NODELAY选项 1:启用 0:不启用（缺省） */
+					/* Enable TCP_NODELAY option 1:enable 0:disable(DEFAULT) */
+	int		tcp_linger ;	/* 启用TCP_LINGER选项 1:启用并设置成参数值-1值 0:不启用（缺省） */
+					/* Enable TCP_LINGER option 1:enable 0:disable(DEFAULT) */
+	
 	/* 以下为内部使用 */
+	/* The following for internal use */
 	int		install_winservice ;
 	int		uninstall_winservice ;
 } ;
